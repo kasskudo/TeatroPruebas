5dbe75efafaa6e02fc55ea5723e63bbc05cdcda9
AlbertoMaza <berto.colindres>
2020-03-09 19:55:05 +0100

    ClasesTest Alberto

--- test/modelos/TesTsuite1.java
+++ test/modelos/TesTsuite1.java
@@ -17,7 +17,11 @@ import org.junit.runners.Suite;
  * @author DAW101
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({})
+@Suite.SuiteClasses({
+    
+AcomodadorTest.class
+
+})
 public class TesTsuite1 {
 
     @BeforeClass
